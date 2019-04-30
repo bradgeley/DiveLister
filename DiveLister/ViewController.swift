17400ab6f8968374dcd75d5c9ba1fdfa5e9d5589
bradjc95certitude <bradjc95@gmail.com>
2019-04-30 17:04:31 -0500

    Initial Commit

---
+++ DiveLister/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiveLister
+//
+//  Created by Bradley Christensen on 4/30/19.
+//  Copyright © 2019 Bradley Christensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
